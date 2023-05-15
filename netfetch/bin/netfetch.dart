03441a720b003596d723cdcfed9ed8ca4292d784
cxueying CR <cxueying887@gmail.com>
2023-05-15 02:48:06 +0000

    save

--- netfetch/bin/netfetch.dart
+++ netfetch/bin/netfetch.dart
@@ -2,7 +2,7 @@ import 'package:netfetch/netfetch.dart' as netfetch;
 import 'package:http/http.dart' as http;
 
 void main() async {
-  final httpPackageUrl = Uri.https('github.com', '/cxueying/my_netfetch/blob/main/netfetch/http.json');
+  final httpPackageUrl = Uri.https('github.com', '/cxueying/my_netfetch/main/.json');
   final httpPackageInfo = await http.read(httpPackageUrl);
   print(httpPackageInfo);
 }
